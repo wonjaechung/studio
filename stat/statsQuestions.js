b65ea1033e7f5dd1df85acb95b2a6b68d493c6c8
Wonjae Chung <wchung34@gmail.com>
2025-07-22 17:57:02 +0000

    "중학생들이 이해하기쉽게 통계를 설명해주고싶어.
    스탯교재에서는 보통 population에 대한 parameter 구하는 것이

--- stat/statsQuestions.js
+++ stat/statsQuestions.js
@@ -23,5 +23,32 @@ export const statsQuestions = [
       answerOptions: [ { text: "H₀: p = 0.5 and Hₐ: p > 0.5", isCorrect: true }, { text: "H₀: p = 0.5 and Hₐ: p ≠ 0.5", isCorrect: false }, { text: "H₀: p = 0.5 and Hₐ: p < 0.5", isCorrect: false }, { text: "H₀: p > 0.5 and Hₐ: p ≠ 0.5", isCorrect: false }, { text: "H₀: p > 0.5 and Hₐ: p = 0.5", isCorrect: false } ],
       unit: 'Unit 6: Inference for Proportions', 
       topic: 'Significance Tests for Proportions'
+  },
+    {
+      id: "2016-04", year: 2016, questionNumber: 4,
+      questionText: "A company determines the mean and standard deviation of the number of sick days taken by its employees in one year. Which of the following is the best description of the standard deviation?",
+      answerOptions: [ { text: "Approximately the mean distance between the number of sick days taken by individual employees and the mean number of sick days taken by all employees", isCorrect: true }, { text: "Approximately the median distance between the number of sick days taken by individual employees and the median number of sick days taken by all employees", isCorrect: false }, { text: "The distance between the greatest number of sick days taken by an employee and the mean number of sick days taken by all employees", isCorrect: false }, { text: "The number of days separating the fewest sick days taken and the most sick days taken when considering all employees", isCorrect: false }, { text: "The number of days separating the fewest sick days taken and the most sick days taken when considering the middle 50 percent of the distribution", isCorrect: false } ],
+      unit: 'Unit 1: Exploring One-Variable Data',
+      topic: 'Summarizing Quantitative Data: Measures of Spread'
+  },
+  {
+      id: "2016-05", year: 2016, questionNumber: 5,
+      questionText: "In one region of the country, the mean length of stay in hospitals is 5.5 days with standard deviation 2.6 days. Because many patients stay in the hospital for considerably more days, the distribution of length of stay is strongly skewed to the right. Consider random samples of size 100 taken from the distribution with the mean length of stay, x̄, recorded for each sample. Which of the following is the best description of the sampling distribution of x̄?",
+      answerOptions: [ { text: "Strongly skewed to the right with mean 5.5 days and standard deviation 2.6 days", isCorrect: false }, { text: "Strongly skewed to the right with mean 5.5 days and standard deviation 0.26 day", isCorrect: false }, { text: "Strongly skewed to the right with mean 5.5 days and standard deviation 0.026 day", isCorrect: false }, { text: "Approximately normal with mean 5.5 days and standard deviation 2.6 days", isCorrect: false }, { text: "Approximately normal with mean 5.5 days and standard deviation 0.26 day", isCorrect: true } ],
+      unit: 'Unit 5: Sampling Distributions',
+      topic: 'The Central Limit Theorem (CLT)'
+  },
+  {
+      id: "2016-06", year: 2016, questionNumber: 6,
+      questionText: "A local television news station includes a viewer survey question about a current issue at the beginning of every evening news broadcast. Viewers are invited to use social media to respond to the question. The results of the survey are shared with the audience at the end of each broadcast. In relation to the opinions of the population of the region, which of the following is a possible reason why the results of such surveys could be biased?\n\nI. Viewers with strong opinions about the current issue are more likely to respond than are viewers without strong opinions.\nII. The opinions of viewers of one television station are not necessarily representative of the population of a region.\nIII. Viewers with access to social media are not necessarily representative of the population of a region.",
+      answerOptions: [
+          { text: "(A) I only", isCorrect: false },
+          { text: "(B) II only", isCorrect: false },
+          { text: "(C) III only", isCorrect: false },
+          { text: "(D) II and III only", isCorrect: false },
+          { text: "(E) I, II, and III", isCorrect: true }
+      ],
+      unit: 'Unit 3: Collecting Data',
+      topic: 'Potential Problems with Sampling'
   }
 ];
